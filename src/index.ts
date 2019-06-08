edfc4f9911c34643a4a9481f8c79c093490d57e8
Shootacean <shootacean@gmail.com>
2019-06-08 22:53:20 +0900

    [add] key bind

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,37 @@ import DrumSet from "./drumSet";
 
 window.onload = () => {
 
+  function playAudio() {
+    if (!drumSet.audio) {
+      const audio = new Audio({
+        bassSource: '/assets/audio/sensessionist_bass_drum.wav',
+      });
+      drumSet.setAudio(audio);
+    }
+    drumSet.playBass();
+  }
+
+  function setEventForPlay(element: HTMLElement) {
+    element.addEventListener('click', () => {
+      playAudio();
+    });
+  }
+
   // Setup drum set
   const drumSet: DrumSet = new DrumSet();
   const playingDrumSet = document.getElementById('drum-set');
   const bassButton = playingDrumSet.querySelectorAll('.bass');
-  bassButton.forEach((button) => {
-    button.addEventListener('click', () => {
-      if (!drumSet.audio) {
-        const audio = new Audio({
-          bassSource: '/assets/audio/sensessionist_bass_drum.wav',
-        });
-        drumSet.setAudio(audio);
-      }
-      drumSet.playBass();
-    });
+  bassButton.forEach((element: HTMLElement) => {
+    setEventForPlay(element);
+  });
+
+  document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'b':
+      case 'v':
+        playAudio();
+        break;
+    }
   });
 
 };
